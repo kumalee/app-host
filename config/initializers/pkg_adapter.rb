5d5b7a6798cd9cb84f643f18652a3b13f83c7ad2
Kuma Li <li.jun.kuma@gmail.com>
2021-03-17 18:38:56 +0800

    fix androidaab issues

--- config/initializers/pkg_adapter.rb
+++ config/initializers/pkg_adapter.rb
@@ -2,6 +2,6 @@ PkgAdapter.setup do |config|
   config.adapters = {
                     "ios" => {class_name:"Ipa",ext_name:"ipa",des:"iOS"},
                     "android" => {class_name:"Apk",ext_name:"apk",des:"Android Apk"},
-                    "andoridAab" => {class_name:"Aab",ext_name:"aab",des:"Android Aab"}
+                    "androidAppBundle" => {class_name:"Aab",ext_name:"aab",des:"Android Aab"}
                     }
 end
